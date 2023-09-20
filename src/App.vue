944432aa21ea11984c8fac94fc33e5744ba5e2b9
Lawrence L Hovind <lhovind@hovind.com>
2023-09-19 21:17:33 -0400

    Initial code commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h2>Dynamic Scrollable Table Demo</h2>
+    <a href="https://github.com/llhovind/dynamic-scrollable-table" target="_blank" class="githubLink" title="See Me on GitHub"><img src="./assets/github-mark.svg" alt="GitHub" /></a>
+  </header>
+
+  <RouterView />
+
+  <footer>&copy;2022-{{ new Date().getFullYear() }} <a href="http://www.hovind.com" target="_blank">Lawrence L Hovind</a></footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  font-size: smaller;
+  text-align: center;
+  color: gray;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+
+header {
+  display: block;
+  background-image: linear-gradient(to right, rgba(9, 9, 255, .9), rgba(128, 0, 128, .9));
+  color: white;
+  text-align: center;
+  font-size: 1.05rem;
+  padding-bottom: 12px;
+  margin-left: -4px;
+  margin-top: 4px;
+  border: 2px solid gold;
+  border-radius: 6px;
+  box-shadow: 2px 3px 6px gray;
+  position: relative;
+
+  h2 {
+    color: gold;
+    letter-spacing: 2px;
+    font-size: 1.6rem;
+    font-weight: bold;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+  }
+}
+
+a.githubLink {
+  position: absolute;
+  bottom: 0px;
+  right: 6px;
+
+  img {
+    height: 24px;
+    width: 24px;
+  }
+}
+
+@media (min-width: 1024px) {
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script setup>
+import CustomTable from '../components/CustomTable.vue';
+import Explanation from '../components/Explanation.vue';
+</script>
+
+<template>
+  <main>
+    <p>
+    <ul>
+      <li>Hover over a column header to see if it is sortable. Up/Down arrows will appear on sortable columns.</li>
+      <li>Click a sortable column header to change the sort order from Ascending, to Descending, or None.</li>
+      <li>To change column order, Drag-n-Drop a column header onto another header.</li>
+      <li>To change which columns to display, Check/Uncheck the column(s) in the Display Selection.</li>
+    </ul>
+    </p>
+    <CustomTable />
+    <hr />
+    <Explanation />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  align-self: start;
+}
+
+p {
+  position: relative;
+  text-align: center;
+}
+
+ul {
+  display: inline-block;
+  margin: 1rem;
+  padding: 1rem 1rem 1rem 3rem;
+  text-align: left;
+  border: 2px solid lightgreen;
+  border-radius: 12px;
+}
+
+hr {
+  margin: 3rem 1rem 1rem 0.5rem;
+  height: 3px;
+  background-image: linear-gradient(to right, rgba(9, 9, 255, .9), rgba(128, 0, 128, .9));
+  border: none;
+}
+</style>
